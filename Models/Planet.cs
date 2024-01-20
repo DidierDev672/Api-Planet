6b789b871719719eabc4bd30f0a4b68dae2a7e5f
didierarias <$DemonSlayer45@>
2024-01-20 10:29:26 -0500

    Model of the planets

---
+++ Models/Planet.cs
@@ -0,0 +1,8 @@
+public class Planet
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+    public double Mass { get; set; }
+    public double Radius { get; set; }
+    public double DistanceToSun { get; set; }
+}
